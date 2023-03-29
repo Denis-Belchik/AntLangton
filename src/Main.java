22c6937e5010a2133e6a6c4a43197790dfa91774
Setlocal1989 <denis1989g@mail.ru>
2023-03-29 13:23:06 +0400

    Fix

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import AntLangton.AntGame;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         AntGame antGame = new AntGame();
-        antGame.play(30);
+        antGame.play(30000);
     }
 }
